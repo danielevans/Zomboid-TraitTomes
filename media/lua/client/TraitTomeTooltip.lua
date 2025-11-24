cccf14caf8e5a261eb015448c8cdef0fd5c40a50
Daniel Evans <evans.daniel.n@gmail.com>
2025-11-23 21:42:44 -0800

    Fix tooltip rendering

--- media/lua/client/TraitTomeTooltip.lua
+++ media/lua/client/TraitTomeTooltip.lua
@@ -35,20 +35,56 @@ function ISToolTipInv:render()
         text = text .. "Trait: "
         if traitDeltas then
             for _,delta in pairs(traitDeltas) do
-              text = text .. "  " .. delta.op .. delta.label
+              text = text .. "  " .. delta.label
             end
             text = text .. "\n"
         end
 
         local mx = getMouseX() + 24;
         local my = getMouseY() + 24;
+        if not self.followMouse then
+            mx = self:getX()
+            my = self:getY()
+            if self.anchorBottomLeft then
+                mx = self.anchorBottomLeft.x
+                my = self.anchorBottomLeft.y
+            end
+        end
 
-        self.item:setTooltip(text)
-
-        self.tooltip:setX(mx);
+        self.tooltip:setX(mx+11);
         self.tooltip:setY(my);
 
-        self.item:DoTooltip(self.tooltip)
-	end
+        self.tooltip:setWidth(50)
+        self.tooltip:setMeasureOnly(true)
+        self.item:DoTooltip(self.tooltip);
+        self.tooltip:setMeasureOnly(false)
+        local myCore = getCore();
+        local maxX = myCore:getScreenWidth();
+        local maxY = myCore:getScreenHeight();
+
+        local tw = self.tooltip:getWidth();
+        local th = self.tooltip:getHeight();
+
+        self.tooltip:setX(math.max(0, math.min(mx + 11, maxX - tw - 1)));
+        if not self.followMouse and self.anchorBottomLeft then
+            self.tooltip:setY(math.max(0, math.min(my - th, maxY - th - 1)));
+        else
+            self.tooltip:setY(math.max(0, math.min(my, maxY - th - 1)));
+        end
+
+        self:setX(self.tooltip:getX() - 11);
+        self:setY(self.tooltip:getY());
+        self:setWidth(tw + 11);
+        self:setHeight(th);
+
+        if self.followMouse then
+            self:adjustPositionToAvoidOverlap({ x = mx - 24 * 2, y = my - 24 * 2, width = 24 * 2, height = 24 * 2 })
+        end
+
+        self:drawRect(0, 0, self.width, self.height, self.backgroundColor.a, self.backgroundColor.r, self.backgroundColor.g, self.backgroundColor.b);
+        self:drawRectBorder(0, 0, self.width, self.height, self.borderColor.a, self.borderColor.r, self.borderColor.g, self.borderColor.b);
+        self.item:DoTooltip(self.tooltip);
+    end
 end
+
 
